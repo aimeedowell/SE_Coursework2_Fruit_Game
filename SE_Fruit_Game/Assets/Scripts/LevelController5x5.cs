356ac1989aaec1c010c3eaf2509f3b780fa527b5
Aimee Dowell <60100829+aimeedowell@users.noreply.github.com>
2021-12-07 18:34:55 +0000

    Enable user to try level again when failed (level2)

--- SE_Fruit_Game/Assets/Scripts/LevelController5x5.cs
+++ SE_Fruit_Game/Assets/Scripts/LevelController5x5.cs
@@ -37,6 +37,23 @@ public class LevelController5x5 : MonoBehaviour
     GameObject QuestionGenerator;
     GameObject BoardObject;
 
+    int startingScore;
+
+    public void ContinueButtonClicked()
+    {
+        //SceneManager.LoadScene("MathsLevel_3"); // Move to Level 3
+    }
+
+    public void RetryButtonClicked()
+    {
+       Reset();
+    }
+
+    public void ReturnButtonClicked()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -83,6 +100,8 @@ public class LevelController5x5 : MonoBehaviour
         QuestionGenerator = new GameObject();
         QuestionGenerator.AddComponent<ArithmeticQuestionGenerator>();
         QuestionGenerator.GetComponent<ArithmeticQuestionGenerator>().Level = 2;
+
+        startingScore = StaticVariables.Score;
     }
 
     // Update is called once per frame
@@ -90,10 +109,6 @@ public class LevelController5x5 : MonoBehaviour
     {
         if (GameStatus == "InQuestion")
             OnQuestionInputChanged();
-    }
-        public void ReturnButtonClicked()
-    {
-        SceneManager.LoadScene("MainMenu");
     }
     //If GameStatus is TileSelection, on click highlight the selected tile and update GameStatus, SelectedTile and SelectedTileCoords
     public void SelectTile(GameObject obj)
@@ -260,6 +275,7 @@ public class LevelController5x5 : MonoBehaviour
                 {
                     GameStatus = "LevelFailed";                   
                     Debug.Log ("Level Failed");
+                    StaticVariables.Score = startingScore;
                     LevelFailed.SetActive(true);
                 }
                 Text text = SpeechText.GetComponent<Text>();
@@ -301,6 +317,62 @@ public class LevelController5x5 : MonoBehaviour
         SpeechBubble.GetComponent<Image>().CrossFadeAlpha(0.0f, 2.5f, false); //fade out
         SpeechText.GetComponent<Text>().CrossFadeAlpha(0.0f, 2.5f, false); //fade out
 
+    }
+
+    void Reset() 
+    {
+        GameStatus = "TileSelection";
+        CarrotsRemaining = 2;
+        BananasRemaining = 2;
+        BroccoliRemaining = 1;
+        //Make Highlight Square invisible
+        HighlightSquare.SetActive(false);
+        //Make Level Complete Sign invisible 
+        LevelComplete.SetActive(false);
+        //Make Level Failed Sign invisible 
+        LevelFailed.SetActive(false);
+
+        QuestionPopUpManager.GetComponent<QuestionPopUpManager>().HideQuestionPopUp();
+        QuestionPopUpManager.GetComponent<QuestionPopUpManager>().UpdateScoreText();
+
+        //Make all vegetables invisible
+        foreach (GameObject car in CarrotsArray)
+        {
+            car.SetActive(false);
+        }
+        foreach (GameObject ban in BananaArray)
+        {
+            ban.SetActive(false);
+        }
+        foreach (GameObject broc in BroccoliArray)
+        {
+            broc.SetActive(false);
+        }
+
+        for (int i = 0; i < 5; i++ )
+        {
+            TileRow1[i].SetActive(true);
+            TileRow2[i].SetActive(true);
+            if (i != 2) // Avoid empty obj for strawberry
+                TileRow3[i].SetActive(true);
+            TileRow4[i].SetActive(true);
+            TileRow5[i].SetActive(true);
+ 
+        }
+
+
+        Text text = SpeechText.GetComponent<Text>();
+        text.text = SteveQuotes.TileSelection;
+        StartCoroutine(FadeSpeechBubble());
+
+        BoardObject = new GameObject();
+        BoardObject.AddComponent<BoardModel>();
+        BoardObject.GetComponent<BoardModel>().Level = 1;
+
+        QuestionGenerator = new GameObject();
+        QuestionGenerator.AddComponent<ArithmeticQuestionGenerator>();
+        QuestionGenerator.GetComponent<ArithmeticQuestionGenerator>().Level = 1;
+        UpdateVegetablesRemaining();
     }
 
 }
